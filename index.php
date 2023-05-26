5613699dd4f6e45d336582bb480965be4c87c035
Gabriel Carvalho <69277918+k1ngabs@users.noreply.github.com>
2023-05-25 23:30:52 -0300

    css atualização

--- index.php
+++ index.php
@@ -3,22 +3,50 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>MyMachine</title>
-    <link rel="stylesheet" href="components/style.css">
-</head>
-<body>
-    <div class="sidenav">
-
+    <link rel="stylesheet" href="components/style.css" />
+  </head>
+  <body>
+    <header>MyMachine</header>
+    <div class="layout">
+      <div class="sidenav">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
+        consectetur placeat itaque tempore deleniti, ab fuga dolor officia!
+        Dolore voluptate, consequuntur ratione et velit earum obcaecati in
+        dolorum perferendis maxime.
+      </div>
+      <main>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis debitis
+          sit eius provident libero quidem minima vero doloremque earum porro,
+          quaerat molestiae sunt consectetur nostrum facilis. Nesciunt
+          repellendus voluptatibus esse?
+        </div>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis debitis
+          sit eius provident libero quidem minima vero doloremque earum porro,
+          quaerat molestiae sunt consectetur nostrum facilis. Nesciunt
+          repellendus voluptatibus esse?
+        </div>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis debitis
+          sit eius provident libero quidem minima vero doloremque earum porro,
+          quaerat molestiae sunt consectetur nostrum facilis. Nesciunt
+          repellendus voluptatibus esse?
+        </div>
+      </main>
+      <aside>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis debitis
+          sit eius provident libero quidem minima vero doloremque earum porro,
+          quaerat molestiae sunt consectetur nostrum facilis. Nesciunt
+          repellendus voluptatibus esse?
+        </div>
+      </aside>
     </div>
-    <main>
-
-    </main>
-    <aside>
-
-    </aside>
-</body>
+  </body>
 </html>
